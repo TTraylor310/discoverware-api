1e30fe1ba38ce71f540921fa86ff2feae20b1225
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-08 13:32:25 -0500

    Changes schema - images is now an array of image url strings, and removed extra console.logs

--- modules/place.js
+++ modules/place.js
@@ -4,17 +4,17 @@ const mongoose = require('mongoose');
 
 
 const locationSchema = new mongoose.Schema(
-    {
-        email: { type: String, required: true },
-        name: {type: String, required: true},
-        address: {type: String, required: true},
-        lat: {type: Number, required: true},
-        lng: {type: Number, required: true},
-        place_id: {type: String, required: true},
-        image: {type: String},
-        types: {type: Array},
-        notes: {type: String}
-    }
+  {
+      email: { type: String, required: true },
+      name: {type: String, required: true},
+      address: {type: String, required: true},
+      lat: {type: Number, required: true},
+      lng: {type: Number, required: true},
+      place_id: {type: String, required: true},
+      images: {type: Array, required: true},
+      types: {type: Array},
+      notes: {type: String}
+  }
 );
 
 const locationModel = mongoose.model('Location', locationSchema);
